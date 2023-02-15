145fe5d5e8753fc11b5982d939083ab4fdd85eb3
zahoruiko <66906889+zahoruiko@users.noreply.github.com>
2023-02-15 19:26:40 +0100

    Create persons.js

---
+++ persons.js
@@ -0,0 +1,14 @@
+let persons = [
+    {'id': 1,
+    'fullname': 'John Doe',
+    'age': 33,
+    'salary': 2000,
+    'position': 'Developer',
+    },
+    {'id': 1,
+    'fullname': 'Alfred Koch',
+    'age': 19,
+    'salary': 1500,
+    'position': 'Tester',
+    },
+]
